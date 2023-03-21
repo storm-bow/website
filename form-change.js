b4aecab0afeae6085a166de61a282b15ae7af6b1
Yiannis Fertis <>
2023-03-21 17:51:34 +0200

    login-register page modified, listing-page added + js files

--- form-change.js
+++ form-change.js
@@ -4,15 +4,15 @@ const lButtonContainer = document.getElementById("l-button-container");
 const rButtonContainer = document.getElementById("r-button-container");
 const loginForm = document.getElementById("login-form");
 const registerForm = document.getElementById("register-form");
-
+/*
 lButton.addEventListener("click", function onClick(){
     rButtonContainer.style.backgroundColor = 'rgba(169, 169, 169, 0.1)';
     rButton.style.backgroundColor = 'rgba(169, 169, 169, 0)';
     rButtonContainer.style.border = "none";
     rButtonContainer.style.borderBottom = "solid";
+    lButtonContainer.style.backgroundColor = "white";
     lButtonContainer.style.border =  "none";
     lButtonContainer.style.borderRight = "solid";
-    lButtonContainer.style.backgroundColor = "white";
     registerForm.style.display = "none";
     loginForm.style.display = "block";
 });
@@ -27,3 +27,38 @@ rButton.addEventListener("click", function onClick(){
     loginForm.style.display = "none";
     registerForm.style.display = "block";
 });
+*/
+function changeForm(pressedButton) {
+    let otherButton;
+    let pressedButtonContainer;
+    let otherButtonContainer;
+    if(pressedButton===lButton){
+        otherButton = rButton;
+        pressedButtonContainer = lButtonContainer;
+        otherButtonContainer = rButtonContainer;
+        loginForm.style.display = "block";
+        registerForm.style.display = "none";
+    }
+    else{
+        otherButton = lButton;
+        pressedButtonContainer = rButtonContainer;
+        otherButtonContainer = lButtonContainer;
+        loginForm.style.display = "none";
+        registerForm.style.display = "block";
+    }
+    otherButtonContainer.style.backgroundColor = 'rgba(169, 169, 169, 0.1)';
+    otherButton.style.backgroundColor = 'rgba(169, 169, 169, 0)';
+    otherButtonContainer.style.border =  "none";
+    otherButtonContainer.style.borderBottom = "solid";
+    pressedButtonContainer.style.backgroundColor = "white"
+    pressedButtonContainer.style.border= "none";
+    if(pressedButton===lButton){
+        pressedButtonContainer.style.borderRight = "solid";
+    }
+    else{
+        pressedButtonContainer.style.borderLeft = "solid";
+    }
+}
+
+rButton.addEventListener("click", function onClick(){changeForm(rButton)});
+lButton.addEventListener("click", function onClick(){changeForm(lButton)});
