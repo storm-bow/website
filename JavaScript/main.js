fb2971ffc86d8362bb87846fb5bf6848c3b6e65b
bow <>
2023-03-31 23:41:10 +0300

    Many changes, the about page got many new features, the main has a new slideshow.

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,4 +1,3 @@
-
 const arrowAnchor = document.querySelector("#change-section-anchor")
 
 arrowAnchor.addEventListener('mouseover',()=>{
@@ -9,4 +8,18 @@ arrowAnchor.addEventListener('mouseover',()=>{
 arrowAnchor.addEventListener('mouseout',()=>{
     const arrow = document.querySelector("#change-section-anchor")
     arrow.style.color = `rgba(255,255,255,0.6)`;
+})
+
+const section1 = document.querySelector("#section1");
+
+section1.addEventListener('mousemove',(e) => {
+    const vh = window.innerHeight;
+    const y = e.clientY;
+    if(100*y/vh>= 60){
+        arrowAnchor.style.setProperty("display","block","important")
+    }
+    else{
+        arrowAnchor.style.setProperty("display","none","important")
+
+    }
 })
