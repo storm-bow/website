c63f12a428787db9cfef0f2218f5b96314ef6284
bow <>
2023-05-18 23:02:47 +0300

    Changes and fixes

--- Using-Nodejs-Handlebars/database-queries.js
+++ Using-Nodejs-Handlebars/database-queries.js
@@ -1,3 +1,4 @@
+import { connection } from "./database-connector.js";
 
 function showTables(connection){
     connection.execute("SHOW TABLES", function(error,results,fields){
@@ -19,31 +20,42 @@ async function registerUser(connection,first_name,last_name,email,phone,password
     return ret;
 }
 
-async function makeListing(connection,property){
-    console.log(property)
+async function makeListing(connection,property,user){
+    let response = await connection.execute(
+                    `SELECT ID
+                     FROM US3R
+                     WHERE email = '${user.email}'`
+    )
+    let owner = response[0][0].ID;
     let date = new Date().toISOString().split('T')[0]
-    let owner = 1;
     if(property.propertyType == "commercial"){
         await connection.execute(
             `INSERT INTO PROPERTY (ownerID,listingType,country,city,street,streetNumber,firstPublished,lastPublished,state,price,area,floor,numberOfFloors,constrYear,bathrooms,comments)
-             VALUES ('${owner}','${property.listingType}','${property.country}','${property.city}',${property.street},${property.streetNumber},'${date}','${date}','in-process',${property.price},${property.area},${property.floor},${property.numberOfFloors},${property.constrYear},${property.bathrooms},'${property.comments}')`
+             VALUES ('${owner}','${property.listingType}','${property.country}','${property.city}','${property.street}',${property.streetNumber},'${date}','${date}','in-process',${property.price},${property.area},${property.floor},${property.numberOfFloors},${property.constrYear},${property.bathrooms},'${property.comments}')`
             )
-        let propertyID = await connection.execute(
-            `SELECT ID from PROPERTY (ID,ownerID)
+
+        let propertyID
+        
+        await connection.execute(
+            `SELECT ID 
+             FROM PROPERTY
              WHERE ownerID = ${owner}
-             ORDER BY 'ID' DESC
+             ORDER BY ID DESC
              LIMIT 1`
-            ).results
-        console.log(propertyID)
+            ).then((data) => {
+                console.log(data[0][0].ID)
+                propertyID = data[0][0].ID
+            })
         await connection.execute(
             `INSERT INTO COMMERCIAL (cpropertyID,commercialType)
-             VALUES (${propertyID},${property.commercialType}})`
+             VALUES (${propertyID},'${property.commercialType}')`
             )
+
     }
     if(property.propertyType == "residential"){
         await connection.execute(
             `INSERT INTO PROPERTY (ownerID,listingType,country,city,street,streetNumber,firstPublished,lastPublished,state,price,area,floor,numberOfFloors,constrYear,bathrooms,comments)
-             VALUES ('${owner}','${property.listingType}','${property.country}','${property.city}','${property.street}',${property.streetNumber},'${date}','${date}','in-process',${property.price},${property.area},${property.floor},${property.numberOfFloors},${property.constrYear},${property.bathrooms},'${property.comments}')`
+             VALUES ('${owner}','${property.listingType}','${property.country}','${property.city}','${property.street}',${property.streetNumber},'${date}','${date}','in-process',${property.price},${property.area},'${property.floor}','${property.numberOfFloors}','${property.constrYear}',${property.bathrooms},'${property.comments}')`
             )
         
         let propertyID
@@ -55,7 +67,6 @@ async function makeListing(connection,property){
              ORDER BY ID DESC
              LIMIT 1`
             ).then((data) => {
-                console.log(data[0][0].ID)
                 propertyID = data[0][0].ID
             })
 
@@ -68,5 +79,23 @@ async function makeListing(connection,property){
     }
 } 
 
+async function checkUser(connection,email){
 
-export {showTables, registerUser, makeListing}
+    let response = await connection.execute(
+        `SELECT EXISTS ( SELECT * FROM US3R WHERE email = '${email}')`
+    )
+    let exists = Object.values(response[0][0])[0] // If a user with this email exists it returns 1, else 0
+    return exists;
+}
+
+async function getUser(connection,email){
+    let response = await connection.execute(
+        `SELECT first_name,last_name,email,phone,password 
+         FROM US3R
+         WHERE email = '${email}'`
+    )
+    console.log(response[0][0])
+    return response[0][0]
+}
+
+export {showTables, registerUser, makeListing, checkUser, getUser}
--- Using-Nodejs-Handlebars/routing.js
+++ Using-Nodejs-Handlebars/routing.js
@@ -6,43 +6,116 @@ import { body, validationResult } from "express-validator"
 import asyncHandler from "express-async-handler"
 import bodyParser from 'body-parser';
 import multer from "multer"
+import bcrypt from 'bcrypt'
 
 const router = express.Router();
 
 const upload = multer({ dest: 'uploads/' })
 
 router.get("/main", (req, res) => {
-    res.render("nothing", { layout: "main" })
+    if(req.session.authenticatedUser){
+        res.render("nothing",{layout:"main",user: req.session.authenticatedUser})
+    }
+    else{
+        res.render("nothing", { layout: "main",user: false })
+    }
 }
 )
 router.get("/about-us", (req, res) => {
-    res.render("creators", { layout: "about-us", creators: basicinfo.creators })
-})
-router.get("/login", (req, res) => {
-    res.render("nothing", { layout: "login" })
-})
-router.get("/register", (req, res) => {
-    res.render("nothing", { layout: "register" })
-})
-router.get("/make-listing", (req, res) => {
-    if(req.query.type === "residential"){
-        res.render("rooms",{layout: "make-listing-page", type: "residential"})
+    if(req.session.authenticatedUser){
+        res.render("creators", { layout: "about-us", creators: basicinfo.creators, user: req.session.authenticatedUser })
     }
     else{
-        res.render("commercialType",{layout: "make-listing-page", type: "commercial"})
+        res.render("creators", { layout: "about-us", creators: basicinfo.creators })
     }
+})
+router.get("/login", (req, res) => {
+    if(req.session.authenticatedUser){
+        res.render("nothing", { layout: "login",user: req.session.authenticatedUser})
+    }
+    else{
+        res.render("nothing", { layout: "login" })
+    }
+})
+router.get("/register", (req, res) => {
+    if(req.session.authenticatedUser){
+        res.render("nothing", { layout: "register", user: req.session.authenticatedUser})
+    }
+    else{
+        res.render("nothing", { layout: "register" })
+    }})
+router.get("/make-listing", (req, res) => {
+    if (req.query.type === "residential") {
+        if(req.session.authenticatedUser){
+            res.render("rooms", { layout: "make-listing-page", type: "residential", user: req.session.authenticatedUser })
+        }
+        else{
+            res.redirect("main")
+        }
+    }
+    else {
+        if(req.session.authenticatedUser){
+            res.render("commercial", { layout: "make-listing-page", type: "commercial", user: req.session.authenticatedUser })
+        }
+        else{
+            res.redirect("main")
+        }
+    }  
 
 })
 
+//LOGIN
+router.post("/login", asyncHandler(async (req, res, next) => {
+    //Authenticate User
+
+    let check = await dbquery.checkUser(connection, req.body.email); // checks if a user with this emal exists
+    if (check === 0) {
+        res.render("nothing", { layout: "login", error: [{ msg: "Error: Wrong email or password" }] })
+    }
+    else {
+        let user = await dbquery.getUser(connection, req.body.email)
+        let result = await bcrypt.compare(req.body.password, user.password);
+        if (result) {
+            // επιτυχία, αποθηκεύομε το username στις μεταβλητές συνεδρίας
+            // ώστε να ξέρουμε πως έγινε η σύνδεση
+            req.session.authenticatedUser = {
+                first_name: user.first_name,
+                last_name: user.last_name,
+                phone: user.phone,
+                email: user.email
+            }
+            res.redirect('/main')
+        }
+        else {
+            // αποτυχία, ανακατεύθυνση στη φόρμα εισόδου για νέα δοκιμή
+            res.render("nothing", { layout: "login", error: [{ msg: "Error: Wrong email or password" }] })
+        }
+    }
+}))
+
+//LOG-OUT
+router.get('/logout', (req, res) => {
+    if (req.session) {
+      req.session.destroy(err => {
+        if (err) {
+          res.status(400).send('Unable to log out')
+        } else {
+          res.redirect('/main')
+        }
+      });
+    } else {
+      res.end()
+    }
+  })
 //REGISTER USER
 router.post(
     "/register",
     body("first_name").trim().isLength({ min: 1 }).escape(),
     body("last_name").trim().isLength({ min: 1 }).escape(),
     body("email").trim().escape(),
-    body("phone").trim().isLength({min:10, max:10}).escape().withMessage("Error: phone number should be 10 numbers"),
-    body("password").trim().isLength({min: 8}).escape().withMessage("Error: password must be at least 8 characters"),
-    body("vpassword").trim().escape(),
+    body("phone").trim().isLength({ min: 10, max: 10 }).escape().withMessage("Error: phone number should be 10 numbers"),
+    body("password").trim().isLength({ min: 8 }).escape().withMessage("Error: password must be at least 8 characters"),
+    body("vpassword").trim().isLength({ min: 8, max: 80 }).escape(),
     asyncHandler(async (req, res, next) => {
         // Extract the validation errors from a request.
         const errors = validationResult(req);
@@ -64,27 +137,31 @@ router.post(
             });
             return;
         } else {
-            let newEntry = await dbquery.registerUser(connection,req.body.first_name,req.body.last_name,req.body.email,req.body.phone,req.body.password)
-            if(newEntry===false){
-                console.log("problem")
-                res.render("nothing",{layout: "register", error: [{msg: "Error: this email is already in use."}]})
-            }
-            else if(req.body.password != req.body.vpassword){
-                res.render("nothing",{layout: "register", error: [{msg:"Error: passwords don't match"}]})
+            if (req.body.password != req.body.vpassword) {
+                res.render("nothing", { layout: "register", error: [{ msg: "Error: passwords don't match" }] })
 
             }
-            else{
-                // Data from form is valid.
-                console.log("Saved")
-                // Save data.
-                //await author.save();
-                // Redirect to completion page
-                res.redirect("/main");
+            else {
+                req.body.password = bcrypt.hashSync(req.body.password, 10)
+                let newEntry = await dbquery.registerUser(connection, req.body.first_name, req.body.last_name, req.body.email, req.body.phone, req.body.password)
+                if (newEntry === false) {
+                    console.log("problem")
+                    res.render("nothing", { layout: "register", error: [{ msg: "Error: this email is already in use." }] })
+                }
+                else {
+                    // Data from form is valid.
+                    console.log("Saved")
+                    // Save data.
+                    //await author.save();
+                    // Redirect to completion page
+                    res.redirect("/main");
+                }
+
             }
 
         }
     }
-        
+
     )
 )
 //MAKE LISTING
@@ -92,26 +169,27 @@ router.post(
     "/make-listing",
     body("propertyType").trim().escape(),
     body("listingType").trim().escape(),
-    body("country").trim().isLength({ min: 1, max:30 }).escape(),
-    body("city").trim().isLength({ min: 1, max:30 }).escape(),
-    body("street").trim().isLength({ min: 1, max:30 }).escape(),
+    body("country").trim().isLength({ min: 1, max: 30 }).escape(),
+    body("city").trim().isLength({ min: 1, max: 30 }).escape(),
+    body("street").trim().isLength({ min: 1, max: 30 }).escape(),
     body("streetNumber").trim().escape(),
     body("floor").trim().escape(),
     body("numberOfFloors").trim().escape(),
     body("bathrooms").trim().escape(),
     body("rooms").trim().escape(),
-    body("commercialType").trim().isLength({ min: 1, max:25 }).escape(),
+    body("commercialType").trim().isLength({ min: 1, max: 25 }).escape(),
     body("constrYear").trim().escape(),
     body("area").trim().escape(),
     body("price").trim().escape(),
-    body("comments").trim().isLength({ max:200 }).escape(),
+    body("comments").trim().isLength({ max: 200 }).escape(),
     upload.single("photo"),
-    asyncHandler(async (req,res,next) => {
+    asyncHandler(async (req, res, next) => {
         const errors = validationResult(req);
         console.log(errors)
         let property = req.body
-        property["photo"]= req.file.path
-        await dbquery.makeListing(connection,property)
+        property["photo"] = req.file.path
+        let user = req.session.authenticatedUser
+        await dbquery.makeListing(connection, property, user)
         res.redirect("/make-listing")
     })
 
